565f9f176d101f8be9a11d23f2f38d0dc7aa8157
Jorge Vilela <jorge.vilela2006@gmail.com>
2026-03-04 11:28:10 -0300

    Add comparison of boolean values with prompts

--- Questao7.js
+++ Questao7.js
@@ -1,1 +1,17 @@
 const prompt = require('prompt-sync')();
+
+let valor1 = prompt("Digite seu valor (true/false) ").toLowerCase();
+let valor2 = prompt("Digite seu valor (true/false) ").toLowerCase(); 
+let valoresiguais = valor1 === valor2;
+let valoresDiferentes = valor1 != valor2;
+
+if (valoresiguais) {
+    console.log("Os valores são iguais? "+ valoresiguais);   
+}else {
+    console.log("Os valores são diferentes? "+ valoresDiferentes);
+}
+
+
+
+
+
